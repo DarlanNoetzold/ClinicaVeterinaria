62629a3091d8f81ff3f19d561f13ffb3ca79fda3
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-16 09:23:38 -0300

    Create TipoProduto.java

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/clinicaVet/model/TipoProduto.java
@@ -0,0 +1,5 @@
+package br.edu.ifsul.cc.lpoo.clinicaVet.model;
+
+public enum TipoProduto {
+    MEDICAMENTO, SESSAO_FISIOTERAPIA, SESSAO_ADESTRAMENTO, ATENDIMENTO_AMBULATORIAL, CONSULTA, CONSULTA_REVISAO
+}
