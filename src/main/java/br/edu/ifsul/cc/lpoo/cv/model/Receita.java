6db2fdf796760029f57d04c5c2c624e834f25446
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-17 06:13:00 -0300

    Create Receita.java

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Receita.java
@@ -0,0 +1,61 @@
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_receita")
+public class Receita {
+
+    @Id
+    @SequenceGenerator(name = "seq_receita", sequenceName = "seq_receita_id", allocationSize = 1)
+    @GeneratedValue(generator = "seq_receita", strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String orientacao;
+
+    @ManyToOne
+    @JoinColumn(name = "consulta_id", nullable = false)
+    private Consulta consulta;
+
+    @ManyToMany
+    @JoinTable(name = "tb_receita_produto", joinColumns = {@JoinColumn(name = "receita_id")},
+            inverseJoinColumns = {@JoinColumn(name = "produto_id")})
+    private List<Produto> produtos;
+
+    public Receita() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOrientacao() {
+        return orientacao;
+    }
+
+    public void setOrientacao(String orientacao) {
+        this.orientacao = orientacao;
+    }
+
+    public Consulta getConsulta() {
+        return consulta;
+    }
+
+    public void setConsulta(Consulta consulta) {
+        this.consulta = consulta;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+}
