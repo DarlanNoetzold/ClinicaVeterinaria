595635b8297b02facdc30c4b7344945aed1f2552
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-17 06:12:05 -0300

    Create Fornecedor.java

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Fornecedor.java
@@ -0,0 +1,37 @@
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_fornecedor")
+@DiscriminatorValue("O")
+public class Fornecedor extends Pessoa{
+
+    @Column(nullable = false)
+    private String cnpj;
+
+    @Column(nullable = false)
+    private String ie;
+
+    public Fornecedor() {
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getIe() {
+        return ie;
+    }
+
+    public void setIe(String ie) {
+        this.ie = ie;
+    }
+}
