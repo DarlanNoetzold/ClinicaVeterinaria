a20b0e86a159822976b5f456f675063ce7a83543
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-17 06:13:02 -0300

    Create TipoProduto.java

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/TipoProduto.java
@@ -0,0 +1,5 @@
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+public enum TipoProduto {
+    MEDICAMENTO, SESSAO_FISIOTERAPIA, SESSAO_ADESTRAMENTO, ATENDIMENTO_AMBULATORIAL, CONSULTA, CONSULTA_REVISAO
+}
