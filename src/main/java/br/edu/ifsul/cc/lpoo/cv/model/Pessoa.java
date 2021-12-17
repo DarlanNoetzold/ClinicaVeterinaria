176a9ba017147c6b689df88525e0a89d400e4462
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-17 06:12:49 -0300

    Create Pessoa.java

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Pessoa.java
@@ -0,0 +1,124 @@
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Entity
+@Table(name = "tb_jogador")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "tipo")
+public class Pessoa {
+    @Id
+    private String cpf;
+
+    @Column(nullable = false)
+    private String rg;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String numero_celular;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar data_nascimento;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column(nullable = false)
+    private String complemento;
+
+    public Pessoa() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNumero_celular() {
+        return numero_celular;
+    }
+
+    public void setNumero_celular(String numero_celular) {
+        this.numero_celular = numero_celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Calendar getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Calendar data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
