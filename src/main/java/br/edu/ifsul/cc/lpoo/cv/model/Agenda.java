4f94718d4450f8f1b52520bdcbb3d512ffce9487
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-17 06:10:39 -0300

    Update Agenda.java

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Agenda.java
@@ -0,0 +1,96 @@
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Entity
+@Table(name = "tb_agenda")
+public class Agenda {
+    @Id
+    @SequenceGenerator(name = "seq_agenda", sequenceName = "seq_agenda_id", allocationSize = 1)
+    @GeneratedValue(generator = "seq_agenda", strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar data_inicio;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar data_fim;
+
+    @Column(nullable = false)
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id", nullable = false)
+    private Medico medico;
+
+    @ManyToOne
+    @JoinColumn(name = "funcionario_id", nullable = false)
+    private Funcionario funcionario;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TipoProduto tipoProduto;
+
+    public Agenda() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Calendar getData_inicio() {
+        return data_inicio;
+    }
+
+    public void setData_inicio(Calendar data_inicio) {
+        this.data_inicio = data_inicio;
+    }
+
+    public Calendar getData_fim() {
+        return data_fim;
+    }
+
+    public void setData_fim(Calendar data_fim) {
+        this.data_fim = data_fim;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public TipoProduto getTipoProduto() {
+        return tipoProduto;
+    }
+
+    public void setTipoProduto(TipoProduto tipoProduto) {
+        this.tipoProduto = tipoProduto;
+    }
+}
