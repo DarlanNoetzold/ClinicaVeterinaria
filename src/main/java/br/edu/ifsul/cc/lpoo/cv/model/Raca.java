abfbc8f9d2f65f2c33dd6064286c5082e9b0bc43
Darlan Noetzold <darlan.noetzold@gmail.com>
2021-12-17 06:12:57 -0300

    Create Raca.java

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Raca.java
@@ -0,0 +1,47 @@
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_raca")
+public class Raca {
+
+    @Id
+    @SequenceGenerator(name = "seq_raca", sequenceName = "seq_raca_id", allocationSize = 1)
+    @GeneratedValue(generator = "seq_raca", strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "especie_id", nullable = false)
+    private Especie especie;
+
+    public Raca() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Especie getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(Especie especie) {
+        this.especie = especie;
+    }
+}
