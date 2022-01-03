36c37f1e43cf9ed4779d71844759723682673851
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-01-03 12:32:17 -0300

    Teste de persistencia, find

--- src/test/java/br/edu/ifsul/cc/lpoo/cs/test/TestPersistenciaJDBC.java
+++ src/test/java/br/edu/ifsul/cc/lpoo/cs/test/TestPersistenciaJDBC.java
@@ -1,8 +1,11 @@
 package br.edu.ifsul.cc.lpoo.cs.test;
 
 import br.edu.ifsul.cc.lpoo.cv.model.Consulta;
+import br.edu.ifsul.cc.lpoo.cv.model.Receita;
 import br.edu.ifsul.cc.lpoo.cv.model.dao.PersistenciaJDBC;
 import org.junit.Test;
+
+import java.util.List;
 
 public class TestPersistenciaJDBC {
 
@@ -25,10 +28,25 @@ public class TestPersistenciaJDBC {
     public void testListPersistenciaConsulta() throws Exception {
         PersistenciaJDBC persistencia = new PersistenciaJDBC();
 
-        Consulta consulta = (Consulta) persistencia.find(Consulta.class, 5);
-
-
-
+        if(persistencia.conexaoAberta()){
+            List<Consulta> lista = persistencia.listPesistenciaConsulta();
+            if(!lista.isEmpty()){
+                for(Consulta c : lista){
+                    System.out.println("----Dados de consulta----\n" +
+                            "\nId: " + c.getId() +
+                            "\nCPF do Médico: "+c.getMedico().getCpf()+
+                            "\nCRMV do Médico: "+c.getMedico().getNumero_crmv()+
+                            "\nNome do Médico: "+c.getMedico().getNome()+
+                            "\nId do PET: "+c.getPet().getId()+
+                            "\nNome do PET: "+c.getPet().getNome()+
+                            "\nReceitas desta Consulta: ");
+                    c.getReceitas().stream().forEach(r -> System.out.println("Receita{" +
+                            "id=" + r.getId() +
+                            ", orientacao='" + r.getOrientacao()+ '\'' +
+                            '}'));
+                }
+            }
+        }
     }
 
 
