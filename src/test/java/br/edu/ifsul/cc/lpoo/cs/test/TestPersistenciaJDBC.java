c60549ecf0bc04ff512b666bdf797e1d2edf0ae2
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-01-06 11:16:46 -0300

    Teste de persistência de pessoa e ajustes

--- src/test/java/br/edu/ifsul/cc/lpoo/cs/test/TestPersistenciaJDBC.java
+++ src/test/java/br/edu/ifsul/cc/lpoo/cs/test/TestPersistenciaJDBC.java
@@ -4,6 +4,7 @@ import br.edu.ifsul.cc.lpoo.cv.model.*;
 import br.edu.ifsul.cc.lpoo.cv.model.dao.PersistenciaJDBC;
 import org.junit.Test;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class TestPersistenciaJDBC {
@@ -54,6 +55,43 @@ public class TestPersistenciaJDBC {
 
                 }
             }else{
+                System.out.println("Nenhum dado encontrado, inciando incersão!");
+                Pessoa pessoaMedico = new Pessoa();
+                pessoaMedico.setTipo("M");
+                pessoaMedico.setCep("45678912");
+                pessoaMedico.setComplemento("Eh isso ai mesmo");
+                pessoaMedico.setNome("João");
+                pessoaMedico.setCpf("00000000000");
+                pessoaMedico.setData_nascimento(Calendar.getInstance());
+                pessoaMedico.setEmail("medico@mail.com");
+                pessoaMedico.setEndereco("Algum lugar, numero 0, Rua");
+                pessoaMedico.setNumero_celular("5499999999");
+                pessoaMedico.setRg("55555555555");
+                pessoaMedico.setSenha("123456");
+
+                persistencia.persist(pessoaMedico);
+
+                Pessoa pessoaCliente = new Pessoa();
+                pessoaCliente.setTipo("C");
+                pessoaCliente.setCep("45678912");
+                pessoaCliente.setComplemento("Eh isso ai mesmo");
+                pessoaCliente.setNome("Maria");
+                pessoaCliente.setCpf("11111111111");
+                pessoaCliente.setData_nascimento(Calendar.getInstance());
+                pessoaCliente.setEmail("medico@mail.com");
+                pessoaCliente.setEndereco("Algum lugar, numero 0, Rua");
+                pessoaCliente.setNumero_celular("5499999999");
+                pessoaCliente.setRg("55555555555");
+                pessoaCliente.setSenha("123456");
+
+                persistencia.persist(pessoaCliente);
+
+                Cliente cliente = new Cliente();
+                cliente.setCpf("11111111111");
+                cliente.setData_ultima_visita(Calendar.getInstance());
+
+                persistencia.persist(pessoaCliente);
+
 
             }
         }
