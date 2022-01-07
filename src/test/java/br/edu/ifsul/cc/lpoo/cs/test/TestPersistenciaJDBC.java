5ed73cf95baf5db073345d75351ca99e9bf33aa6
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-01-07 11:33:20 -0300

    Update TestPersistenciaJDBC.java

--- src/test/java/br/edu/ifsul/cc/lpoo/cs/test/TestPersistenciaJDBC.java
+++ src/test/java/br/edu/ifsul/cc/lpoo/cs/test/TestPersistenciaJDBC.java
@@ -98,6 +98,74 @@ public class TestPersistenciaJDBC {
 
                 persistencia.persist(medico);
 
+                Especie especie = new Especie();
+                especie.setNome("Cachorro");
+
+                persistencia.persist(especie);
+
+                Raca raca = new Raca();
+                raca.setNome("Poodle");
+                raca.setEspecie(((Especie) persistencia.find(Especie.class, 1)));
+
+
+                persistencia.persist(raca);
+
+                Pet pet = new Pet();
+                pet.setRaca((Raca) persistencia.find(Raca.class, 1));
+                Calendar data_nasc = Calendar.getInstance();
+                data_nasc.set(2015,10,25);
+                pet.setData_nascimento(data_nasc);
+                pet.setCliente((Cliente) persistencia.find(Cliente.class, "11111111111"));
+                pet.setObservacao("Esquema vacinal");
+                pet.setNome("Galadriel");
+
+                persistencia.persist(pet);
+
+                //Consulta 01
+                Consulta consulta1 = new Consulta();
+                consulta1.setMedico((Medico) persistencia.find(Medico.class, "00000000000"));
+                consulta1.setPet((Pet) persistencia.find(Pet.class, 1));
+                persistencia.persist(consulta1);
+
+                //Consulta 02
+                Consulta consulta2 = new Consulta();
+                consulta2.setMedico((Medico) persistencia.find(Medico.class, "00000000000"));
+                consulta2.setPet((Pet) persistencia.find(Pet.class, 1));
+                persistencia.persist(consulta2);
+
+                //Receitas da consulta 01
+                Receita receita1 = new Receita();
+                receita1.setConsulta((Consulta) persistencia.find(Consulta.class, 1));
+                receita1.setOrientacao("Vacina 01");
+                persistencia.persist(receita1);
+                Receita receita2 = new Receita();
+                receita2.setConsulta((Consulta) persistencia.find(Consulta.class, 1));
+                receita2.setOrientacao("Vacina 02");
+                persistencia.persist(receita2);
+                Receita receita3 = new Receita();
+                receita3.setConsulta((Consulta) persistencia.find(Consulta.class, 1));
+                receita3.setOrientacao("Vacina 03");
+                persistencia.persist(receita3);
+                Receita receita4 = new Receita();
+                receita4.setConsulta((Consulta) persistencia.find(Consulta.class, 1));
+                receita4.setOrientacao("Vacina 04");
+                persistencia.persist(receita4);
+
+                //Receitas da consulta 02
+                Receita receita5 = new Receita();
+                receita5.setConsulta((Consulta) persistencia.find(Consulta.class, 2));
+                receita5.setOrientacao("Anti vermes 01");
+                persistencia.persist(receita5);
+                Receita receita6 = new Receita();
+                receita6.setConsulta((Consulta) persistencia.find(Consulta.class, 2));
+                receita6.setOrientacao("Anti vermes 02");
+                persistencia.persist(receita6);
+                Receita receita7 = new Receita();
+                receita7.setConsulta((Consulta) persistencia.find(Consulta.class, 2));
+                receita7.setOrientacao("Anti vermes 03");
+                persistencia.persist(receita7);
+
+
 
             }
         }
