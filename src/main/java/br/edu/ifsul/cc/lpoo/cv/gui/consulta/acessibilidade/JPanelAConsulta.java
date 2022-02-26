3e7f0de5c86c373f28f63a39aff46af245d3e633
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-02-26 11:34:16 -0300

    Update JPanelAConsulta.java

--- src/main/java/br/edu/ifsul/cc/lpoo/cv/gui/consulta/acessibilidade/JPanelAConsulta.java
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/gui/consulta/acessibilidade/JPanelAConsulta.java
@@ -11,7 +11,7 @@ public class JPanelAConsulta extends JPanel {
     private CardLayout cardLayout;
     private Controle controle;
     
-    private JPanelAConsultaFormulario formulario;
+    private JPanelAConsultaFormulario consulta;
     private JPanelAConsultaListagem listagem;
     
     public JPanelAConsulta(Controle controle){
@@ -25,24 +25,29 @@ public class JPanelAConsulta extends JPanel {
         cardLayout = new CardLayout();
         this.setLayout(cardLayout);
         
-        formulario = new JPanelAConsultaFormulario(this, controle);
+        consulta = new JPanelAConsultaFormulario(this, controle);
         listagem = new JPanelAConsultaListagem(this, controle);
         
-        this.add(formulario, "tela_consulta_formulario");
+        this.add(consulta, "tela_consulta_formulario");
         this.add(listagem, "tela_consulta_listagem");
         
         cardLayout.show(this, "tela_consulta_listagem");
     }
     
     public void showTela(String nomeTela){
-        
+        if(nomeTela.equals("tela_consulta_listagem")){
+            listagem.populaTable();
+        }
         cardLayout.show(this, nomeTela);
     }
 
     public Controle getControle() {
         return controle;
     }
-    
-    
-    
+
+    public JPanelAConsultaFormulario getConsulta() {
+        return consulta;
+    }
+
+
 }
